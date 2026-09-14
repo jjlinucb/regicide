2f3829295fdee106dec81615e8ed2cb5d125f64f
John L <jjlinucb@berkeley.edu>
2026-09-13 21:02:57 -0700

    Release Regicide v2.0

--- packages/client/src/pages/HomePage.tsx
+++ packages/client/src/pages/HomePage.tsx
@@ -73,7 +73,7 @@ export function HomePage({
   return (
     <div className="centered-page">
       <h1>
-        Regicide <span className="version-tag">v1.5</span>
+        Regicide <span className="version-tag">v2.0</span>
       </h1>
       <div className="legacy-mode-toggle">
         <button type="button" className={!isLegacy ? 'btn' : 'btn btn-secondary'} onClick={() => setMode('regicide')}>
